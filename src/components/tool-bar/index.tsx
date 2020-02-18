a9fb4df522dce763cc8f97e155c16e066e6929e5
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-02-18 11:23:40 +0100

    chore(): remove unused buttons

--- src/components/tool-bar/index.tsx
+++ src/components/tool-bar/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { setPullRequestFilter } from "store/pullRequestFilter";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import NotificationBar from "components/notfication-bar";
+import { State, PullRequest } from "utils/types";
+import { Wrapper } from "./tool-bar.styled";
+// import watch from "redux-watch";
+// import store from "store";
 
 interface Props {}
 
@@ -11,15 +16,35 @@ const ToolBar: React.FC<Props> = () => {
     dispatch(setPullRequestFilter(filter));
   };
 
+  const numberOfNotifications: number = useSelector(
+    (rootState: State) =>
+      rootState.pullRequests.reviewRequestedPullRequests.filter(
+        ({ isNewReviewRequest }: PullRequest) => isNewReviewRequest
+      ).length
+  );
+  // let w = watch(store.getState, "pullRequestFilter.userInvolvementType");
+  // store.subscribe(
+  //   w((newVal, oldVal, objectPath) => {
+  //     console.log("%s changed from %s to %s", objectPath, oldVal, newVal);
+  //   })
+  // );
+
   return (
-    <div>
+    <Wrapper>
       <button onClick={() => setFilter("author")}>CREATED</button>
-      {/* <button onClick={() => setFilter("assignee")}>ASSIGNED</button>
-      <button onClick={() => setFilter("involves")}>MENTIONED</button> */}
-      <button onClick={() => setFilter("review-requested")}>
-        REVIEW REQUESTED
-      </button>
-    </div>
+
+      <Wrapper>
+        <button onClick={() => setFilter("review-requested")}>
+          REVIEW REQUESTED
+        </button>
+        {numberOfNotifications > 0 && (
+          <NotificationBar
+            status="ACCEPTED"
+            numberOfNotifications={numberOfNotifications}
+          />
+        )}
+      </Wrapper>
+    </Wrapper>
   );
 };
 
