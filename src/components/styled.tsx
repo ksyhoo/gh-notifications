f342a3d8cf8777886683f0c98cc5ea8bc8a40f1e
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-02-18 11:21:55 +0100

    chore(): removed unued style

--- src/components/styled.tsx
+++ src/components/styled.tsx
@@ -14,16 +14,3 @@ export const Container = styled.main`
   margin-left: auto;
   margin-right: auto;
 `;
-
-export const Logo = styled.h1`
-  font-size: 3em;
-  text-transform: uppercase;
-  color: ${(_) => _.theme.colors.ui};
-  margin: 48px 32px;
-  span {
-    color: ${(_) => _.theme.colors.primary};
-  }
-  @media (max-width: 800px) {
-    text-align: center;
-  }
-`;
