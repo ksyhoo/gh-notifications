26e0f5dd0ff9e728ec933d4194943858345d70cf
Krzysztof Kulikowski <krzysztof.kulikowski@netguru.com>
2020-02-20 15:39:13 +0100

    feat(): store methods simplified

--- src/store/pullRequests.ts
+++ src/store/pullRequests.ts
@@ -3,24 +3,15 @@ import { PullRequestsState, PullRequest } from "utils/types";
 import { AppThunk } from "store";
 import {
   searchPullRequestsGql,
-  normalizeGqlResponse,
-  getAuthorPullRequests,
-  getToReviewPullRequests,
   mockData,
   normalizeGqlResponseToObject
 } from "utils/helpers";
 import deepEqual from "deep-equal";
 
-import { GraphQLNormalizr } from "graphql-normalizr";
-
-// const config = ...
-const { normalize } = new GraphQLNormalizr({});
-
 const initialState: PullRequestsState = {
   isLoading: false,
   error: null,
-  createdPullRequests: [],
-  reviewRequestedPullRequests: []
+  items: {}
 };
 
 export const pullRequestsSlice = createSlice({
@@ -29,10 +20,9 @@ export const pullRequestsSlice = createSlice({
   reducers: {
     getPullRequestsSuccess: (
       state,
-      { payload }: PayloadAction<PullRequest[]>
+      { payload }: PayloadAction<{ [id: string]: PullRequest }>
     ) => {
-      state.createdPullRequests = getAuthorPullRequests(payload);
-      state.reviewRequestedPullRequests = getToReviewPullRequests(payload);
+      state.items = payload;
     },
     getPullRequestsLoading: (state, { payload }: PayloadAction<boolean>) => {
       state.isLoading = payload;
@@ -43,42 +33,24 @@ export const pullRequestsSlice = createSlice({
     ) => {
       state.error = payload;
     },
-
-    checkForUpdatesAuthorPullRequests: (
+    checkForUpdates: (
       state,
-      { payload }: PayloadAction<PullRequest[]>
+      { payload }: PayloadAction<{ [id: string]: PullRequest }>
     ) => {
-      state.createdPullRequests.forEach((pullRequest: PullRequest) => {
-        const toCompare = payload.find(
-          (payloadItem: PullRequest) => payloadItem.id === pullRequest.id
-        );
-        const index = state.createdPullRequests.findIndex(
-          (item: PullRequest) => item.id === pullRequest.id
-        );
-        if (deepEqual(toCompare, pullRequest)) {
-          state.createdPullRequests[index] = pullRequest;
+      for (var item in payload) {
+        if (!deepEqual(state.items[item], payload[item])) {
+          state.items[item] = { ...payload[item], hasChanged: true };
+        } else {
+          // TODO: hande equality
         }
-        state.createdPullRequests[index] = toCompare as PullRequest;
-      });
-    },
-    checkForUpdatesReviewRequestedPullRequests: (
-      state,
-      { payload }: PayloadAction<PullRequest[]>
-    ) => {
-      const newRequests = checkForNewReviewRequests(
-        payload,
-        state.reviewRequestedPullRequests
-      );
-
-      state.reviewRequestedPullRequests = newRequests;
+      }
     }
   }
 });
 
 const { actions } = pullRequestsSlice;
 export const {
-  checkForUpdatesAuthorPullRequests,
-  checkForUpdatesReviewRequestedPullRequests,
+  checkForUpdates,
   getPullRequestsSuccess,
   getPullRequestsError,
   getPullRequestsLoading
@@ -89,56 +61,27 @@ export const fetchPullRequests = (): AppThunk => async (dispatch) => {
     dispatch(getPullRequestsLoading(true));
     const pullRequests = await searchPullRequestsGql();
     if (pullRequests) {
-      const asd = normalizeGqlResponseToObject(pullRequests.search.edges, "id");
-      console.log(asd);
-
-      dispatch(
-        getPullRequestsSuccess(normalizeGqlResponse(pullRequests.search.edges))
+      const normalized = normalizeGqlResponseToObject(
+        pullRequests.search.edges,
+        "id"
       );
+      dispatch(getPullRequestsSuccess(normalized));
     }
   } catch (err) {
-    console.log(err);
+    console.warn(err);
     dispatch(getPullRequestsError(err.toString()));
   }
   dispatch(getPullRequestsLoading(false));
 };
 
-//FIXME: remove mocked data with api response
+// FIXME: remove mocked data with api response
 export const checkForUpdatesInStore = (data?: any): AppThunk => async (
   dispatch
 ) => {
   try {
-    dispatch(
-      checkForUpdatesAuthorPullRequests(getAuthorPullRequests(mockData))
-    );
-    dispatch(
-      checkForUpdatesReviewRequestedPullRequests(
-        getToReviewPullRequests(mockData)
-      )
-    );
+    const normalized = normalizeGqlResponseToObject(mockData, "id");
+    dispatch(checkForUpdates(normalized));
   } catch (err) {
     dispatch(getPullRequestsError(err.toString()));
   }
-};
-
-//FIXME: Apply store updating values
-const checkForNewReviewRequests = (
-  data: any, // this should be fetched new data
-  reviewRequestedPullRequests: PullRequest[]
-) => {
-  if (data.length > reviewRequestedPullRequests.length) {
-    const modified = data.map((item: PullRequest) => {
-      const found = reviewRequestedPullRequests.find(
-        (pr: PullRequest) => pr.id === item.id
-      );
-      if (!found) {
-        return { ...item, isNewReviewRequest: true };
-      }
-      return found;
-    });
-    return modified;
-  } else if (data.length < reviewRequestedPullRequests.length) {
-    return data;
-  }
-  return reviewRequestedPullRequests;
 };
